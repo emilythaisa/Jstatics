303e3d85c0d1504839285d97fe8dfe4a0c25796e
emilythaisa <emytonna@gmail.com>
2025-05-08 11:42:38 -0300

    Aulafinalizada

---
+++ src/Mamifero.java
@@ -0,0 +1,16 @@
+public class Mamifero {
+    //atributos
+    private int id;
+    private String nomeCientifico;
+
+    public int getId(){
+        return this.id;
+    }
+    public void setNomeCientifico(String nomeCientifico){
+        this.nomeCientifico = nomeCientifico;
+    }
+    public String getNomeCientifico (){
+        return this.nomeCientifico;
+    }
+
+}
