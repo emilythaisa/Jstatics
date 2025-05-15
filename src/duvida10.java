2e6d91f57d1398b94986e60a7ea2a859f60750da
emilythaisa <emytonna@gmail.com>
2025-05-15 11:56:51 -0300

    finalizado

--- src/duvida10.java
+++ src/duvida10.java
@@ -1,13 +1,25 @@
+import javax.swing.JOptionPane;
+
 public class duvida10 {
-    public static void main (String args){
-        double areaJardim1 = 200;
-        double areaJardim2 = 300;
-        double areaJardim3 = 500;
+
+    public static void main (String[] args){
+        //double areaJardim1 = 200;
+        //double areaJardim2 = 300;
+        //double areaJardim3 = 500;
 
         double [] areasJardins = {200,300,500};
         System.out.println(areasJardins[0]);
         System.out.println(areasJardins[1]);
         System.out.println(areasJardins[2]);
-        // se for 20 jardins
+        double acumuladora = 0;
+
+        for(int i =0;i<areasJardins.length;i++){
+        JOptionPane.showMessageDialog(null, areasJardins [i]);
+        acumuladora = acumuladora + areasJardins[i];
+        }
+
+        JOptionPane.showMessageDialog(null, "quantidade de áreas:"+areasJardins);
+        double media = acumuladora/areasJardins.length;
+        JOptionPane.showMessageDialog(null, "média"+media);
     }
 }
